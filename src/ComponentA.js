f422563a7c01ee15f740896146b856647bf481b6
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-20 12:17:28 +0530

    initial Project setup | cleaned up unnecessary code | added required code

---
+++ src/ComponentA.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ComponentA extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>ComponentA</h2>
+      </>
+    )
+  }
+}
+
+export default ComponentA;
---
+++ src/ComponentB.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ComponentB extends React.Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
+
+export default ComponentB;
