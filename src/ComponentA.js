cae6b4f4c8bbb2e34c0c6ec5d8670511d5e8132b
Gagan-Sky <gagankumar8294@gmail.com>
2024-07-20 20:18:11 +0530

    added promise based fetch function to test dummy test data | sideEffects creating in componentDidMount

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -2,12 +2,32 @@ import React from 'react';
 
 class ComponentA extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      name: "Component A",
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => this.setState({ data: data }))
+  }
   
 
   render() {
     return (
       <>
-        <h2>ComponentA</h2>
+        <h1>{this.state.name}</h1>
+        <ul>
+          {this.state.data.map((d) => {
+            return (
+              <li>{d.username}</li>
+            )
+          })}
+        </ul>
       </>
     )
   }
