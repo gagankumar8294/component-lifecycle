baf67bfeb117f277b9f97ce52e1d41929f8347ad
Gagan-Sky <gagankumar8294@gmail.com>
2024-08-05 18:12:52 +0530

    unknown

--- src/ComponentB.js
+++ src/ComponentB.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class ComponentB extends React.Component {
+
+  constructor() {
+    super()
+    this.state = ({
+      data : [],
+    })
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => this.setState({ data : data }))
+  }
+
   render() {
     return (
-      <>
-        
+      <><ul>
+        {this.state.data.map((data , index) => {
+            return (
+            <li><h1 key={index}>{data.username}</h1></li>
+            )
+        })}</ul>
       </>
     )
   }
